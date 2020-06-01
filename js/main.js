09bc7bd510bfa2ec142462df9192a15fe74e6abd
Khoa <tealkrysta92@gmail.com>
2020-06-01 10:22:12 -0700

    latest stuff

--- js/main.js
+++ js/main.js
@@ -132,6 +132,7 @@ document.addEventListener("DOMContentLoaded", function () {
             fetchApi();
         } else {
             drop.value = 'Filters';
+            fetchApi();
         }
     })
 
